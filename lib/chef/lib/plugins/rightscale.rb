d4e3aef33f7e4c0f5732dd332bacf62003b47252
caryp <cary@rightscale.com>
2009-10-16 11:01:57 -0700

    added custom ohai plugins directory

---
+++ lib/chef/lib/plugins/rightscale.rb
@@ -0,0 +1,125 @@
+#
+# Copyright (c) 2009 RightScale Inc
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# = NOTICE
+# This plugin is deprecated!
+#
+# This is an ohai plugin that checks the state of the instance to
+# determine if the RightScale plaform is available and add RightScale
+# configuration data to the chef node.  This is the old way of doing things.
+#
+# Going forward we are passing in all RightScale configuration data via
+# attributes called 'core-defined inputs'.  You can read more on the wiki:
+# http://rightscale.onconfluence.com/display/rightscale/Cloud+Independent+Attributes
+#
+# This plugin remains until all configuration data is migrated from user-data to
+# core-defined inputs.
+
+require 'json'
+ 
+provides "rightscale"
+
+class RightScale
+  MetaDataFile = "/var/spool/rackspace/user-data.txt"  
+end
+
+# Adds RightScale server FQDNs to the rightscale_deprecated servers Mash
+#
+# NOTE: This is a hack to convert the RS_<server> tokens into something more 
+# intuative.  Hopefully this will be removed when we stop using EC2
+# userdata. 
+#
+# === Parameters
+# key<String>:: RightScale server token from user-data
+# data<String>::: FQDN for the RightScale server
+def add_server(key, data)
+  rightscale_deprecated[:server] = Mash.new unless rightscale_deprecated.has_key?(:server)
+  server_names = {
+    "RS_sketchy" => "sketchy",
+    "RS_syslog" => "syslog",
+    "RS_lumberjack" => "lumberjack",
+    "RS_server" => "core"
+  }
+  rightscale_deprecated[:server][server_names[key]] = data unless (server_names[key] == nil)
+end
+
+# ----------------------------------------
+# ec2
+# ----------------------------------------
+require_plugin "ec2"
+
+# Detect if RightScale platform is running on ec2 cloud
+#
+# === Returns
+# true:: If ec2 Mash exists amd RightScale tokens found in user-data
+# false:: Otherwise
+def on_rightscale_ec2_platform?
+  return false if (ec2 == nil || ec2[:userdata].match(/RS_/) == nil) # only ec2 supported
+  true
+end
+
+# add all 'RS_' tokens in userdata, but perform translation for server names
+def get_data_from_ec2_user_date
+  data_array = ec2[:userdata].split('&')
+  data_array.each do |d|
+    key, data = d.split('=')
+    rightscale_deprecated[key.sub(/RS_/,'')] = data unless add_server(key,data) 
+  end
+end
+
+# Adds rightscale_deprecated from ec2 user-data, if available 
+if on_rightscale_ec2_platform?
+  rightscale_deprecated Mash.new
+  get_data_from_ec2_user_date 
+end
+
+
+# ----------------------------------------
+# generic cloud
+# ----------------------------------------
+
+# Detect if RightScale platform is running on other cloud.
+# currently only rackspace is supported.
+#
+# === Returns
+# true:: If ec2 Mash exists amd RightScale tokens found in user-data
+# false:: Otherwise
+def on_rightscale_platform?
+  File.exists?(RightScale::MetaDataFile)
+end
+
+# add all 'RS_' tokens in medadata file, but perform translation for server names
+def get_data
+  data_array = File.open(RightScale::MetaDataFile)
+  data_array.each do |d|
+    key, data = d.split('=')
+    key.strip!
+    data.strip!
+    rightscale_deprecated[key.sub(/RS_/,'')] = data unless add_server(key,data) 
+  end
+end
+
+# Adds rightscale_deprecated from metadata file, if available 
+if on_rightscale_platform?
+  rightscale_deprecated Mash.new
+  get_data
+end  
---
+++ lib/chef/spec/rightscale_spec.rb
@@ -0,0 +1,157 @@
+#
+# Copyright (c) 2009 RightScale Inc
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+require File.join(File.dirname(__FILE__), '..', '..', '/spec_helper.rb')
+require 'flexmock'
+ 
+describe Ohai::System, "plugin rightscale" do
+  before(:each) do
+    @ohai = Ohai::System.new
+    @ohai.stub!(:require_plugin).and_return(true)
+  end
+  
+  #
+  # EC2 Cloud Support
+  #
+  describe "ec2 with RightScale platform" do
+    before(:each) do
+      @ohai[:ec2] = Mash.new()
+      @ohai[:ec2][:userdata] = "RS_api_url=https:\/\/my.rightscale.com\/api\/inst\/ec2_instances\/c18c07eb8d33456db3d75fe65c56e8bae94d8f59&RS_server=my.rightscale.com&RS_sketchy=sketchy1-12.rightscale.com&RS_token=e668bb0be59d061b8b60f08765902a90"
+    end
+    
+    it "should create rightscale mash" do
+      @ohai._require_plugin("rightscale")
+      @ohai[:rightscale].should_not be_nil
+    end
+    
+    it "should create rightscale server mash" do
+      @ohai._require_plugin("rightscale")
+      @ohai[:rightscale][:server].should_not be_nil
+    end
+    
+    it "should populate sketchy server attribute" do
+      @ohai._require_plugin("rightscale")
+      @ohai[:rightscale][:server]['sketchy'].should == "sketchy1-12.rightscale.com"
+    end
+    
+    it "should populate core server attribute" do
+      @ohai._require_plugin("rightscale")
+      @ohai[:rightscale][:server][:core].should == "my.rightscale.com"
+    end
+    
+    it "should populate token attribute" do
+      @ohai._require_plugin("rightscale")
+      @ohai[:rightscale][:token].should == "e668bb0be59d061b8b60f08765902a90"
+    end
+    
+  end
+
+ describe "ec2 without rightscale" do
+    before(:each) do
+      @ohai[:ec2] = Mash.new()
+      @ohai[:ec2][:userdata] = "some other form of userdata"
+    end
+   
+    it "should NOT populate the rightscale data" do
+      @ohai._require_plugin("rightscale")
+      @ohai[:rightscale].should be_nil
+    end
+  end
+  
+  #
+  # Generic Cloud Support
+  #
+  describe "cloud without rightscale" do
+    before(:each) do
+      @ohai[:cloud] = Mash.new()
+    end
+   
+    it "should NOT populate the rightscale data" do
+      @ohai._require_plugin("rightscale")
+      @ohai[:rightscale].should be_nil
+    end
+  end
+  
+  describe "cloud with RightScale platform" do
+    before(:each) do
+      @ohai[:cloud] = Mash.new()
+      @ohai[:cloud][:provider] = "rackspace"
+      File.stub!(:exists?).and_return(true)
+      @mock_file = mock("datafile")
+      @mock_file.stub!(:each).
+          # json input file (future?)
+          #and_yield('{"rightscale":{"token":"8bd736d4a8de91b143bcebbb3e513f5f","server":{"syslog":"syslog.rightscale.com","sketchy":"sketchy1-11.rightscale.com","core":"my.rightscale.com","lumberjack":"lumberjack.rightscale.com"},"api_url":"https:\/\/my.rightscale.com\/api\/inst\/ec2_instances\/40e9d3956ad2e059f9f4054c6272ce2a38155273"}}')
+          and_yield('RS_syslog=syslog.rightscale.com').
+          and_yield('RS_sketchy=sketchy1-11.rightscale.com').
+          and_yield('RS_server=my.rightscale.com').
+          and_yield('RS_lumberjack=lumberjack.rightscale.com').
+          and_yield('RS_src=dobedobedo').
+          and_yield('RS_token=8bd736d4a8de91b143bcebbb3e513f5f').
+          and_yield('RS_api_url=https://my.rightscale.com/api/inst/ec2_instances/40e9d3956ad2e059f9f4054c6272ce2a38155273').
+          and_yield('RS_token_id=blabla').
+          and_yield('RS_amqp_url=yakityyakyak')       
+        File.stub!(:open).and_return(@mock_file) 
+    end
+    
+    it "should create rightscale mash" do     
+      @ohai._require_plugin("rightscale")
+      @ohai[:rightscale].should_not be_nil
+    end
+    
+    it "should create rightscale server mash" do
+      @ohai._require_plugin("rightscale")
+      @ohai[:rightscale][:server].should_not be_nil
+    end
+         
+    it "should populate sketchy server attribute" do
+      @ohai._require_plugin("rightscale")
+      @ohai[:rightscale][:server][:sketchy].should == "sketchy1-11.rightscale.com"
+    end
+
+    it "should populate core server attribute" do
+      @ohai._require_plugin("rightscale")
+      @ohai[:rightscale][:server][:core].should == "my.rightscale.com"
+    end
+
+    it "should populate syslog server attribute" do
+      @ohai._require_plugin("rightscale")
+      @ohai[:rightscale][:server][:syslog].should == "syslog.rightscale.com"
+    end
+
+    it "should populate lumberjack server attribute" do
+      @ohai._require_plugin("rightscale")
+      @ohai[:rightscale][:server][:lumberjack].should == "lumberjack.rightscale.com"
+    end
+
+    it "should populate token attribute" do
+      @ohai._require_plugin("rightscale")
+      @ohai[:rightscale][:token].should == "8bd736d4a8de91b143bcebbb3e513f5f"
+    end
+    
+    it "should populate api_url attribute" do
+      @ohai._require_plugin("rightscale")
+      @ohai[:rightscale][:api_url].should == "https://my.rightscale.com/api/inst/ec2_instances/40e9d3956ad2e059f9f4054c6272ce2a38155273"
+    end
+    
+  end
+  
+end
