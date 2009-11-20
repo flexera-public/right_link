f2ee4cbd6aabb4b05100cb40f8e46fd876fcf829
Raphael Simon <raphael@rightscale.com>
2009-11-19 18:40:50 -0800

    Remove non-working implementation for debugger support

--- scripts/lib/agent_controller.rb
+++ scripts/lib/agent_controller.rb
@@ -57,7 +57,6 @@
 #      --pid-dir DIR:       Pid files directory (/tmp by default)
 #      --foreground, -f:    Run agent in foreground
 #      --interactive, -I:   Spawn an irb console after starting agent
-#      --debugger, -D PORT  Start a debug server on PORT and immediately break
 #      --test:              Use test settings
 #      --version, -v:       Display version information
 #      --help:              Display help
@@ -223,10 +222,6 @@ module RightScale
           options[:console] = true
         end
 
-        opts.on("-D", "--debugger PORT") do |port|
-          options[:debugger] = port.to_i 
-        end
-
         opts.on("--help") do
           RDoc::usage_from_file(__FILE__)
         end
@@ -310,8 +305,6 @@ module RightScale
 
       puts "#{name} started."
 
-      start_debugger(@options[:debugger]) if @options[:debugger]
-
       EM.run do
         Nanite.start_agent(@options)
       end
@@ -335,17 +328,6 @@ module RightScale
         show(pid_file) if pid_file = agent_pid_file(@options[:agent])
       else
         show(agent_pid_file_from_id(@options, id))
-      end
-    end
-
-    # Start a debug server listening on the specified port
-    def start_debugger(port)
-      begin
-        require 'ruby-debug'
-        require 'ruby-debug-ide'
-        Debugger::start_server('127.0.0.1', port)
-      rescue LoadError => e
-        raise LoadError.new("Cannot start debugger unless ruby-debug and ruby-debug-ide gems are available")
       end
     end
 
