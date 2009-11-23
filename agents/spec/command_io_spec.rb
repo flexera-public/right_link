dac9bc4beb15df12c482819164950ced878b9c54
Scott Messier <scott@rightscale.com>
2009-11-23 13:03:16 -0800

    fix for command_io_spec.rb failing under windows

--- agents/spec/command_io_spec.rb
+++ agents/spec/command_io_spec.rb
@@ -1,5 +1,5 @@
-require File.join(File.dirname(__FILE__), '..', '..', 'spec', 'spec_helper')
-require File.join(File.dirname(__FILE__), '..', '..', 'lib', 'command_protocol', 'lib', 'command_protocol')
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'spec', 'spec_helper'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'lib', 'command_protocol', 'lib', 'command_protocol'))
 require 'command_io'
 require 'exceptions'
 require 'ostruct'
@@ -28,11 +28,8 @@ describe RightScale::CommandIO do
   it 'should receive a command' do
     @input = ''
     EM.run do
-      # note the windows is sensitive to event timing, so use next_tick
-      EM.next_tick do
-        RightScale::CommandIO.listen { |input, _| @input = input; stop }
-        send_input('input')
-      end
+      RightScale::CommandIO.listen { |input, _| @input = input; stop }
+      send_input('input')
       EM.add_timer(2) { stop }
     end
     @input.should == 'input'
@@ -41,19 +38,17 @@ describe RightScale::CommandIO do
   it 'should receive many commands' do
     @inputs = []
     EM.run do
-      # note the windows is sensitive to event timing, so use next_tick
-      EM.next_tick do
-        RightScale::CommandIO.listen do |input, _|
-           @inputs << input
-           stop if input == 'final'
-        end
-        for i in 1..50 do
-          send_input("input#{i}")
-        end
-        send_input("final")
+      RightScale::CommandIO.listen do |input, _|
+        @inputs << input
+        stop if input == 'final'
       end
+      for i in 1..50 do
+        send_input("input#{i}")
+      end
+      send_input("final")
       EM.add_timer(2) { stop }
     end
+
     @inputs.size.should == 51
     (0..49).each { |i| @inputs[i].should == "input#{i+1}" }
     @inputs[50].should == 'final'
