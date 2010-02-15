4341e7018afabc20a22f055c46b4c6012f852cc2
Raphael Simon <raphael@rightscale.com>
2010-02-15 15:53:16 -0800

    Fix auditor proxy specs

--- agents/spec/auditor_proxy_spec.rb
+++ agents/spec/auditor_proxy_spec.rb
@@ -31,19 +31,19 @@ describe RightScale::AuditorProxy do
   end
 
   it 'should log and audit errors' do
-    flexmock(RightScale::RightLinkLog).should_receive(:error).once.with("AUDIT *ERROR> ERROR")
+    flexmock(RightScale::RightLinkLog).should_receive(:error).once.with("*ERROR> ERROR")
     @instance.should_receive(:push).once.and_return { |*_| EM.stop }
     EM.run { @proxy.append_error('ERROR') }
   end
 
   it 'should log statuses' do
-    flexmock(RightScale::RightLinkLog).should_receive(:info).once.with("AUDIT *RS> STATUS")
+    flexmock(RightScale::RightLinkLog).should_receive(:info).once.with("*RS> STATUS")
     @instance.should_receive(:push).once.and_return { |*_| EM.stop }
     EM.run { @proxy.update_status('STATUS') }
   end
 
 it 'should log outputs' do
-    flexmock(RightScale::RightLinkLog).should_receive(:info).once.with("AUDIT OUTPUT").and_return { |*_| EM.stop }
+    flexmock(RightScale::RightLinkLog).should_receive(:info).once.with("OUTPUT").and_return { |*_| EM.stop }
     @instance.should_receive(:push).once
     EM.run do
       EM.add_timer(RightScale::AuditorProxy::MAX_AUDIT_DELAY + 1) { EM.stop }
@@ -52,13 +52,14 @@ it 'should log outputs' do
   end
 
   it 'should log sections' do
-    flexmock(RightScale::RightLinkLog).should_receive(:info).once.with("AUDIT #{ '****' * 20 }\n*RS>#{ 'SECTION'.center(72) }****")
+    flexmock(RightScale::RightLinkLog).should_receive(:info).once.ordered.with("#{ '****' * 20 }")
+    flexmock(RightScale::RightLinkLog).should_receive(:info).once.ordered.with("*RS>#{ 'SECTION'.center(72) }****")
     @instance.should_receive(:push).once.and_return { |*_| EM.stop }
     EM.run { @proxy.create_new_section('SECTION') }
   end
 
   it 'should log information' do
-    flexmock(RightScale::RightLinkLog).should_receive(:info).once.with("AUDIT *RS> INFO")
+    flexmock(RightScale::RightLinkLog).should_receive(:info).once.with("*RS> INFO")
     @instance.should_receive(:push).once.and_return { |*_| EM.stop }
     EM.run { @proxy.append_info('INFO') }
   end
