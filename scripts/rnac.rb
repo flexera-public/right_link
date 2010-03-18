fa2443f42bc181560b3a6b7c85de87d10638a8ee
Raphael Simon <raphael@rightscale.com>
2010-03-18 11:13:56 -0700

    Define HOME environment variable if it's missing to make package installer happy

--- scripts/rnac.rb
+++ scripts/rnac.rb
@@ -25,6 +25,10 @@
 #
 # See lib/agent_controller.rb for additional information.
 
+# Monit cleans the environment before running a daemon.  
+# We can re-set any necessary environment here.
+ENV['HOME'] = "/root" unless ENV['HOME'] # Needed by package installer
+
 THIS_DIR = File.dirname(File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__)
 $:.push(File.join(THIS_DIR, 'lib'))
 
