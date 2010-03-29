78d390664522d7e42582d994d028b087047f87b4
Raphael Simon <raphael@rightscale.com>
2010-03-29 11:53:03 -0700

    Change umask to match default

--- common/lib/common/daemonize.rb
+++ common/lib/common/daemonize.rb
@@ -25,11 +25,11 @@ module RightScale
     def daemonize(identity, options = {})
       exit if fork
       Process.setsid
+      File.umask 0022
       exit if fork
       STDIN.reopen "/dev/null"
       STDOUT.reopen "#{options[:log_path]}/nanite.#{identity}.out", "a"
       STDERR.reopen "#{options[:log_path]}/nanite.#{identity}.err", "a"
-      File.umask 0000
     end
   end
 end
