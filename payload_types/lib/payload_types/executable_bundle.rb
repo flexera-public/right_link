389d606e82946ee588809b38337d47f05ac8229c
Graham Hughes <graham@rightscale.com>
2010-10-11 17:28:42 -0700

    Add repose_server to the executable bundle.

--- payload_types/lib/payload_types/executable_bundle.rb
+++ payload_types/lib/payload_types/executable_bundle.rb
@@ -51,17 +51,21 @@ module RightScale
     # (Array) Chef cookbooks
     attr_accessor :cookbooks
 
+    # (String) Repose server to use
+    attr_accessor :repose_server
+
     def initialize(*args)
       @executables           = args[0]
       @cookbook_repositories = args[1] if args.size > 1
       @audit_id              = args[2] if args.size > 2
       @full_converge         = args[3] if args.size > 3
       @cookbooks             = args[4] if args.size > 4
+      @repose_server         = args[5] if args.size > 5
     end
 
     # Array of serialized fields given to constructor
     def serialized_members
-      [ @executables, @cookbook_repositories, @audit_id, @full_converge, @cookbooks ]
+      [ @executables, @cookbook_repositories, @audit_id, @full_converge, @cookbooks, @repose_server ]
     end
 
     # Human readable representation
