78dded22af84a91cdb8ee37e708ed4029458e64c
Lee Kirchhoff <lee@rightscale.com>
2010-12-14 08:12:37 -0800

    Fix stats reporting for activities when rate not being measured

--- common/lib/common/stats_helper.rb
+++ common/lib/common/stats_helper.rb
@@ -179,6 +179,7 @@ module RightScale
           end
           result.merge!("last" => last)
           result.merge!("rate" => avg_rate) if @measure_rate
+          result
         end
       end
 
