6506aff35d133e47c2c77083b2c770f1f22428e9
Jon Seaberg <jon@rightscale.com>
2011-08-11 16:22:17 -0700

    add global scope to ensure LinuxBlockDevice is found

--- lib/chef/plugins/linux/block_device2.rb
+++ lib/chef/plugins/linux/block_device2.rb
@@ -21,4 +21,4 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 provides "block_device2"
-block_device RightScale::LinuxBlockDevice.info
+block_device ::RightScale::LinuxBlockDevice.info
