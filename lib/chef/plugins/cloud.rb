7fb7fb4d007a55ac5a67acfd150dea0bb29992d3
Jon Seaberg <jon@rightscale.com>
2011-08-24 16:18:44 -0700

    be more lienent and verbose when fetching metadata.

--- lib/chef/plugins/cloud.rb
+++ lib/chef/plugins/cloud.rb
@@ -39,7 +39,7 @@ begin
   options[:user_metadata] = {:metadata_tree_climber => {:create_leaf_override => lambda { |_, value| value }}}
 
   # log to the ohai log
-  options[:logger] = Ohai::Log
+  options[:logger] = ::RightScale::Log
 
   # create the cloud instance
   cloud_instance = RightScale::CloudFactory.instance.create(RightScale::CloudFactory::UNKNOWN_CLOUD_NAME, options)
@@ -69,6 +69,6 @@ begin
 
 rescue Exception => e
   # cloud was unresolvable, but not all ohai use cases are cloud instances.
-  Ohai::Log.debug(RightScale::Log.format("Cloud was unresolvable", e, :trace))
+  ::RightScale::Log.info(::RightScale::Log.format("Cloud was unresolvable", e, :trace))
   cloud nil
 end
--- spec/clouds/http_metadata_source_spec.rb
+++ spec/clouds/http_metadata_source_spec.rb
@@ -22,7 +22,7 @@
 
 require File.join(File.dirname(__FILE__), 'spec_helper')
 require File.join(File.dirname(__FILE__), 'fetch_runner')
-require File.join(File.dirname(__FILE__), '..', '..', 'lib', 'clouds', 'metadata_sources', 'http_metadata_source')
+require File.join(File.dirname(__FILE__), '..', 'lib', 'clouds', 'metadata_sources', 'http_metadata_source')
 require 'json'
 
 module RightScale
@@ -77,162 +77,217 @@ module RightScale
 end
 
 describe RightScale::MetadataSources::HttpMetadataSource do
+  context "when a metadata tree is available" do
+    before(:each) do
+      @runner = ::RightScale::FetchRunner.new
+      @logger = @runner.setup_log
+      @output_dir_path = File.join(::RightScale::RightLinkConfig[:platform].filesystem.temp_dir, 'rs_raw_metadata_writer_output')
+      setup_metadata_provider
+    end
 
-  before(:each) do
-    @runner = ::RightScale::FetchRunner.new
-    @logger = @runner.setup_log
-    @output_dir_path = File.join(::RightScale::Platform.filesystem.temp_dir, 'rs_raw_metadata_writer_output')
-    setup_metadata_provider
-  end
+    after(:each) do
+      teardown_metadata_provider
+      @logger = nil
+      @runner.teardown_log
+      FileUtils.rm_rf(@output_dir_path) if File.directory?(@output_dir_path)
+      @output_dir_path = nil
+    end
 
-  after(:each) do
-    teardown_metadata_provider
-    @logger = nil
-    @runner.teardown_log
-    FileUtils.rm_rf(@output_dir_path) if File.directory?(@output_dir_path)
-    @output_dir_path = nil
-  end
+    def setup_metadata_provider
+      # source is shared between cloud and user metadata providers.
+      hosts = [:host => ::RightScale::FetchRunner::FETCH_TEST_SOCKET_ADDRESS, :port => ::RightScale::FetchRunner::FETCH_TEST_SOCKET_PORT]
+      @metadata_source = RightScale::MetadataSources::HttpMetadataSource.new(:hosts => hosts, :logger => @logger)
+      cloud_metadata_tree_climber = ::RightScale::MetadataTreeClimber.new(:root_path => ::RightScale::HttpMetadataSourceSpec::METADATA_ROOT.join('/'))
+      user_metadata_tree_climber = ::RightScale::MetadataTreeClimber.new(:root_path => ::RightScale::HttpMetadataSourceSpec::USERDATA_ROOT.join('/'),
+                                                                         :has_children_override => lambda{ false } )
 
-  def setup_metadata_provider
-    # source is shared between cloud and user metadata providers.
-    hosts = [:host => ::RightScale::FetchRunner::FETCH_TEST_SOCKET_ADDRESS, :port => ::RightScale::FetchRunner::FETCH_TEST_SOCKET_PORT]
-    @metadata_source = RightScale::MetadataSources::HttpMetadataSource.new(:hosts => hosts, :logger => @logger)
-    cloud_metadata_tree_climber = ::RightScale::MetadataTreeClimber.new(:root_path => ::RightScale::HttpMetadataSourceSpec::METADATA_ROOT.join('/'))
-    user_metadata_tree_climber = ::RightScale::MetadataTreeClimber.new(:root_path => ::RightScale::HttpMetadataSourceSpec::USERDATA_ROOT.join('/'),
-                                                                       :has_children_override => lambda{ false } )
+      # raw metadata writer.
+      @cloud_raw_metadata_writer = ::RightScale::MetadataWriter.new(:file_name_prefix => ::RightScale::HttpMetadataSourceSpec::METADATA_ROOT.last,
+                                                                    :output_dir_path => @output_dir_path)
+      @user_raw_metadata_writer = ::RightScale::MetadataWriter.new(:file_name_prefix => ::RightScale::HttpMetadataSourceSpec::USERDATA_ROOT.last,
+                                                                   :output_dir_path => @output_dir_path)
 
-    # raw metadata writer.
-    @cloud_raw_metadata_writer = ::RightScale::MetadataWriter.new(:file_name_prefix => ::RightScale::HttpMetadataSourceSpec::METADATA_ROOT.last,
-                                                                  :output_dir_path => @output_dir_path)
-    @user_raw_metadata_writer = ::RightScale::MetadataWriter.new(:file_name_prefix => ::RightScale::HttpMetadataSourceSpec::USERDATA_ROOT.last,
-                                                                 :output_dir_path => @output_dir_path)
+      # cloud metadata
+      @cloud_metadata_provider = ::RightScale::MetadataProvider.new
+      @cloud_metadata_provider.metadata_source = @metadata_source
+      @cloud_metadata_provider.metadata_tree_climber = cloud_metadata_tree_climber
+      @cloud_metadata_provider.raw_metadata_writer = @cloud_raw_metadata_writer
 
-    # cloud metadata
-    @cloud_metadata_provider = ::RightScale::MetadataProvider.new
-    @cloud_metadata_provider.metadata_source = @metadata_source
-    @cloud_metadata_provider.metadata_tree_climber = cloud_metadata_tree_climber
-    @cloud_metadata_provider.raw_metadata_writer = @cloud_raw_metadata_writer
+      # user metadata
+      @user_metadata_provider = ::RightScale::MetadataProvider.new
+      @user_metadata_provider.metadata_source = @metadata_source
+      @user_metadata_provider.metadata_tree_climber = user_metadata_tree_climber
+      @user_metadata_provider.raw_metadata_writer = @user_raw_metadata_writer
+    end
 
-    # user metadata
-    @user_metadata_provider = ::RightScale::MetadataProvider.new
-    @user_metadata_provider.metadata_source = @metadata_source
-    @user_metadata_provider.metadata_tree_climber = user_metadata_tree_climber
-    @user_metadata_provider.raw_metadata_writer = @user_raw_metadata_writer
-  end
+    def teardown_metadata_provider
+      @cloud_metadata_provider = nil
+      @user_metadata_provider = nil
+      @metadata_source.finish
+      @metadata_source = nil
+    end
 
-  def teardown_metadata_provider
-    @cloud_metadata_provider = nil
-    @user_metadata_provider = nil
-    @metadata_source.finish
-    @metadata_source = nil
-  end
+    def verify_cloud_metadata(cloud_metadata)
+      compare_tree = JSON::parse(::RightScale::HttpMetadataSourceSpec::METADATA_TREE.to_json)
+      compare_tree["public-keys"] = compare_tree["public-keys"].dup
+      compare_tree["public-keys"]["0"] = {"openssh-key" => compare_tree["public-keys"]["0=windows_image_build_key"]["openssh-key"].strip}
+      compare_tree["public-keys"].delete_if { |key, value| key == "0=windows_image_build_key" }
 
-  def verify_cloud_metadata(cloud_metadata)
-    compare_tree = JSON::parse(::RightScale::HttpMetadataSourceSpec::METADATA_TREE.to_json)
-    compare_tree["public-keys"] = compare_tree["public-keys"].dup
-    compare_tree["public-keys"]["0"] = {"openssh-key" => compare_tree["public-keys"]["0=windows_image_build_key"]["openssh-key"].strip}
-    compare_tree["public-keys"].delete_if { |key, value| key == "0=windows_image_build_key" }
+      recursive_compare(compare_tree, cloud_metadata)
+    end
 
-    recursive_compare(compare_tree, cloud_metadata)
-  end
+    def recursive_compare(compare_tree, cloud_metadata)
+      compare_tree.each_pair do |key, value|
+        normalized_key = key.gsub(/=.*$/, '/').gsub('-', '_')
+        if value.kind_of?(Hash)
+          recursive_compare(value, cloud_metadata[normalized_key])
+        else
+          cloud_metadata[normalized_key].should == value
+        end
+      end
+    end
 
-  def recursive_compare(compare_tree, cloud_metadata)
-    compare_tree.each_pair do |key, value|
-      normalized_key = key.gsub(/=.*$/, '/').gsub('-', '_')
-      if value.kind_of?(Hash)
-        recursive_compare(value, cloud_metadata[normalized_key])
+    def verify_user_metadata(user_metadata)
+      user_metadata.should == ::RightScale::HttpMetadataSourceSpec::USERDATA_LEAF
+    end
+
+    def verify_raw_metadata_writer(reader, metadata, subpath = [])
+      if metadata.respond_to?(:has_key?)
+        metadata.each do |key, value|
+          # reversing dash to underscore substitution.  This appears to be valid for the
+          # test data, but will fail if the test data keys ever contain '_'
+          verify_raw_metadata_writer(reader, value, subpath + [key.gsub('_','-')])
+        end
       else
-        cloud_metadata[normalized_key].should == value
+        result = reader.read(subpath)
+        result.strip.should == metadata
       end
     end
-  end
 
-  def verify_user_metadata(user_metadata)
-    user_metadata.should == ::RightScale::HttpMetadataSourceSpec::USERDATA_LEAF
-  end
-
-  def verify_raw_metadata_writer(reader, metadata, subpath = [])
-    if metadata.respond_to?(:has_key?)
-      metadata.each do |key, value|
-        # reversing dash to underscore substitution.  This appears to be valid for the
-        # test data, but will fail if the test data keys ever contain '_' 
-        verify_raw_metadata_writer(reader, value, subpath + [key.gsub('_','-')])
+    it 'should return empty metadata for HTTP calls which return 404s' do
+      cloud_metadata, user_metadata = @runner.run_fetcher(@cloud_metadata_provider, @user_metadata_provider) do |server|
+        # intentionally not mounting metadata
       end
-    else
-      result = reader.read(subpath)
-      result.strip.should == metadata
-    end
-  end
-
-  it 'should return empty metadata for HTTP calls which return 404s' do
-    cloud_metadata, user_metadata = @runner.run_fetcher(@cloud_metadata_provider, @user_metadata_provider) do |server|
-      # intentionally not mounting metadata
-    end
-    cloud_metadata.should == {}
-    user_metadata.should == ""
-  end
-
-  it 'should succeed for successful HTTP calls' do
-    cloud_metadata, user_metadata = @runner.run_fetcher(@cloud_metadata_provider, @user_metadata_provider) do |server|
-      server.recursive_mount_metadata(::RightScale::HttpMetadataSourceSpec::METADATA_TREE, ::RightScale::HttpMetadataSourceSpec::METADATA_ROOT.clone)
-      server.recursive_mount_metadata(::RightScale::HttpMetadataSourceSpec::USERDATA_LEAF, ::RightScale::HttpMetadataSourceSpec::USERDATA_ROOT.clone)
+      cloud_metadata.should == {}
+      user_metadata.should == ""
     end
 
-    verify_cloud_metadata(cloud_metadata)
-    verify_user_metadata(user_metadata)
-    verify_raw_metadata_writer(@cloud_raw_metadata_writer, cloud_metadata)
-    verify_raw_metadata_writer(@user_raw_metadata_writer, user_metadata, nil)
-  end
+    it 'should succeed for successful HTTP calls' do
+      cloud_metadata, user_metadata = @runner.run_fetcher(@cloud_metadata_provider, @user_metadata_provider) do |server|
+        server.recursive_mount_metadata(::RightScale::HttpMetadataSourceSpec::METADATA_TREE, ::RightScale::HttpMetadataSourceSpec::METADATA_ROOT.clone)
+        server.recursive_mount_metadata(::RightScale::HttpMetadataSourceSpec::USERDATA_LEAF, ::RightScale::HttpMetadataSourceSpec::USERDATA_ROOT.clone)
+      end
 
-  it 'should recover from successful HTTP calls which return malformed HTTP response' do
-    requested_branch = false
-    requested_leaf = false
-    cloud_metadata = @runner.run_fetcher(@cloud_metadata_provider) do |server|
-      server.recursive_mount_metadata(::RightScale::HttpMetadataSourceSpec::METADATA_TREE, ::RightScale::HttpMetadataSourceSpec::METADATA_ROOT.clone)
+      verify_cloud_metadata(cloud_metadata)
+      verify_user_metadata(user_metadata)
+      verify_raw_metadata_writer(@cloud_raw_metadata_writer, cloud_metadata)
+      verify_raw_metadata_writer(@user_raw_metadata_writer, user_metadata, nil)
+    end
 
-      # fail a branch request.
-      branch_name = 'block-device-mapping'
-      branch_metadata_path = ::RightScale::HttpMetadataSourceSpec::METADATA_ROOT.clone << branch_name
-      branch_metadata = ::RightScale::HttpMetadataSourceSpec::METADATA_TREE[branch_name]
-      branch_path = server.get_metadata_request_path(branch_metadata_path)
-      branch_response = server.get_metadata_response(branch_metadata)
-      server.unmount(branch_path)
-      server.mount_proc(branch_path) do |request, response|
-        response.body = branch_response
-        unless requested_branch
-          old_status_line = response.status_line
-          response.instance_variable_set(:@injected_status_line, ::RightScale::HttpMetadataSourceSpec::MALFORMED_EC2_HTTP_RESPONSE_PREFIX + old_status_line)
-          def response.status_line
-            return @injected_status_line
+    it 'should recover from successful HTTP calls which return malformed HTTP response' do
+      requested_branch = false
+      requested_leaf = false
+      cloud_metadata = @runner.run_fetcher(@cloud_metadata_provider) do |server|
+        server.recursive_mount_metadata(::RightScale::HttpMetadataSourceSpec::METADATA_TREE, ::RightScale::HttpMetadataSourceSpec::METADATA_ROOT.clone)
+
+        # fail a branch request.
+        branch_name = 'block-device-mapping'
+        branch_metadata_path = ::RightScale::HttpMetadataSourceSpec::METADATA_ROOT.clone << branch_name
+        branch_metadata = ::RightScale::HttpMetadataSourceSpec::METADATA_TREE[branch_name]
+        branch_path = server.get_metadata_request_path(branch_metadata_path)
+        branch_response = server.get_metadata_response(branch_metadata)
+        server.unmount(branch_path)
+        server.mount_proc(branch_path) do |request, response|
+          response.body = branch_response
+          unless requested_branch
+            old_status_line = response.status_line
+            response.instance_variable_set(:@injected_status_line, ::RightScale::HttpMetadataSourceSpec::MALFORMED_EC2_HTTP_RESPONSE_PREFIX + old_status_line)
+            def response.status_line
+              return @injected_status_line
+            end
+            requested_branch = true
           end
-          requested_branch = true
+        end
+
+        # fail a leaf request.
+        leaf_name = 'root'
+        leaf_metadata_path = branch_metadata_path.clone << leaf_name
+        leaf_metadata = branch_metadata[leaf_name]
+        leaf_path = server.get_metadata_request_path(leaf_metadata_path)
+        leaf_response = server.get_metadata_response(leaf_metadata)
+        server.unmount(leaf_path)
+        server.mount_proc(leaf_path) do |request, response|
+          response.body = leaf_response
+          unless requested_leaf
+            old_status_line = response.status_line
+            response.instance_variable_set(:@injected_status_line, ::RightScale::HttpMetadataSourceSpec::MALFORMED_EC2_HTTP_RESPONSE_PREFIX + old_status_line)
+            def response.status_line
+              return @injected_status_line
+            end
+            requested_leaf = true
+          end
         end
       end
+      requested_branch.should == true
+      requested_leaf.should == true
 
-      # fail a leaf request.
-      leaf_name = 'root'
-      leaf_metadata_path = branch_metadata_path.clone << leaf_name
-      leaf_metadata = branch_metadata[leaf_name]
-      leaf_path = server.get_metadata_request_path(leaf_metadata_path)
-      leaf_response = server.get_metadata_response(leaf_metadata)
-      server.unmount(leaf_path)
-      server.mount_proc(leaf_path) do |request, response|
-        response.body = leaf_response
-        unless requested_leaf
-          old_status_line = response.status_line
-          response.instance_variable_set(:@injected_status_line, ::RightScale::HttpMetadataSourceSpec::MALFORMED_EC2_HTTP_RESPONSE_PREFIX + old_status_line)
-          def response.status_line
-            return @injected_status_line
-          end
-          requested_leaf = true
-        end
-      end
+      verify_cloud_metadata(cloud_metadata)
+      verify_raw_metadata_writer(@cloud_raw_metadata_writer, cloud_metadata)
     end
-    requested_branch.should == true
-    requested_leaf.should == true
-
-    verify_cloud_metadata(cloud_metadata)
-    verify_raw_metadata_writer(@cloud_raw_metadata_writer, cloud_metadata)
   end
 
+  context "query" do
+    before(:each) do
+      @log = StringIO.new
+      @logger = Logger.new(@log)
+      @host = "127.0.0.1"
+      @port = 80
+      @mock_uri = flexmock("mock_uri", :host=>@host, :port=>@port, :path=>"foobar", :scheme=>"http")
+      @mock_http_connection = flexmock("mock_connection")
+      flexmock(::Rightscale::HttpConnection).should_receive(:new).and_return(@mock_http_connection)
+      @mock_request = flexmock("mock_request")
+      @mock_request.should_receive(:[]=).with('Connection', 'keep-alive')
+      flexmock(Net::HTTP::Get).should_receive(:new).and_return(@mock_request)
+      flexmock(::RightScale::MetadataSources::HttpMetadataSource).should_receive(:select_metadata_server).and_return([@host, @port])
+      flexmock(::RightScale::MetadataSources::HttpMetadataSource).new_instances.should_receive(:safe_parse_http_uri).and_return(@mock_uri)
+      @http_metadata_source = ::RightScale::MetadataSources::HttpMetadataSource.new(:logger=>@logger, :hosts=>[:host=>@host, :port=>@port])
+    end
+
+    context "when 400 class response from metadata server" do
+      before(:each) do
+        @response = flexmock('Net::HTTPResponse')
+        @response.should_receive(:kind_of?).with(Net::HTTPSuccess).and_return(false)
+        @response.should_receive(:kind_of?).with(Net::HTTPServerError).and_return(false)
+        @response.should_receive(:kind_of?).with(Net::HTTPRedirection).and_return(false)
+        @mock_http_connection.should_receive(:request).once.and_return(@response)
+      end
+      it "should not retry" do
+        pending "TODO"
+      end
+      it "should return empty value" do
+        @http_metadata_source.query("a").should == ""
+      end
+      it "should not stop checking for metadata" do
+        pending "TODO"
+      end
+    end
+
+    context "when 500 class response from metadata server" do
+      before(:each) do
+        @response = flexmock('Net::HTTPResponse')
+        @response.should_receive(:kind_of?).with(Net::HTTPSuccess).and_return(false)
+        @response.should_receive(:kind_of?).with(Net::HTTPServerError).and_return(true)
+        @mock_http_connection.should_receive(:request).times(::RightScale::MetadataSources::HttpMetadataSource::RETRY_MAX_ATTEMPTS).and_return(@response)
+      end
+      it "should retry #{::RightScale::MetadataSources::HttpMetadataSource::RETRY_MAX_ATTEMPTS} times" do
+         pending "TODO"
+      end
+      it "should return empty value after #{::RightScale::MetadataSources::HttpMetadataSource::RETRY_MAX_ATTEMPTS} failed attempts" do
+        @http_metadata_source.query("a").should == ""
+      end
+      it "should not stop checking for metadata" do
+        pending "TODO"
+      end
+    end
+  end
 end
