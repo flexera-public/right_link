7ad692cf878feea45610c593a6dad5472a4342da
Lee Kirchhoff <lee@rightscale.com>
2011-11-11 15:29:36 -0800

    Get cook check_ohai and tag handling running on right thread

--- chef/lib/ohai_setup.rb
+++ chef/lib/ohai_setup.rb
@@ -37,6 +37,10 @@ module RightScale
         Ohai::Config[:plugin_path].unshift(CUSTOM_PLUGINS_DIR_PATH)
         Ohai::Config.log_level RightLinkLog.level
       end
+
+      # must set file cache path and ensure it exists otherwise evented run_command will fail
+      Ohai::Config[:file_cache_path] = RightScale::InstanceConfiguration::CACHE_PATH
+      FileUtils.mkdir_p(Chef::Config[:file_cache_path])
     end
 
     module_function :configure_ohai
--- scripts/lib/cook.rb
+++ scripts/lib/cook.rb
@@ -121,7 +121,8 @@ module RightScale
     # true:: Always return true
     def add_tag(tag_name)
       cmd = { :name => :add_tag, :tag => tag_name }
-      @client.send_command(cmd)
+      # Need to execute on EM main thread where command client is running
+      EM.next_tick { @client.send_command(cmd) }
     end
 
     # Remove given tag from tags exposed by corresponding server
@@ -133,7 +134,8 @@ module RightScale
     # true:: Always return true
     def remove_tag(tag_name)
       cmd = { :name => :remove_tag, :tag => tag_name }
-      @client.send_command(cmd)
+      # Need to execute on EM main thread where command client is running
+      EM.next_tick { @client.send_command(cmd) }
     end
 
     # Access cook instance from anywhere to send requests to core through
