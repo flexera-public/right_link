3ac6553dbd763a98d7f2b7aa9e3b5dac0345091a
Ryan J. Geyer <me@ryangeyer.com>
2011-11-17 12:29:16 -0800

    Stubbed out config drive mounting for windows, better error handling and logging

--- lib/clouds/metadata_sources/config_drive_metadata_source.rb
+++ lib/clouds/metadata_sources/config_drive_metadata_source.rb
@@ -20,16 +20,14 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-# Finding devices (in windows) - http://stackoverflow.com/questions/3258518/ruby-get-available-disk-drives
-
 module RightScale
   module MetadataSources
-    class ConfigDriveMetadataSource < MetadataSource
+    class ConfigDriveMetadataSource < FileMetadataSource
 
       DEFAULT_SYS_BLOCK_DIR_PATH      = "/sys/block"
       DEFAULT_CONFIG_DRIVE_MOUNTPOINT = "/mnt/configdrive"
 
-      attr_accessor :sys_block_dir_path, :config_drive_sector_count, :config_drive_mountpoint, :file_metadata_source
+      attr_accessor :sys_block_dir_path, :config_drive_sector_count, :config_drive_mountpoint
 
       def initialize(options)
         raise ArgumentError, "options[:logger] is required" unless @logger = options[:logger]
@@ -38,31 +36,51 @@ module RightScale
         @config_drive_mountpoint   = options[:config_drive_mountpoint] || DEFAULT_CONFIG_DRIVE_MOUNTPOINT
         @config_drive_sector_count = options[:config_drive_sector_count]
 
-        #@file_metadata_source = ::RightScale::MetadataSources::FileMetadataSource.new(options)
+        super(options.merge({
+          :cloud_metadata_source_file_path => File.join(@config_drive_mountpoint, 'metadata'),
+          :user_metadata_source_file_path => File.join(@config_drive_mountpoint, 'userdata')
+        }))
       end
 
       def query(path)
         mount_config_drive()
+
+        super(path)
       end
 
       private
 
+      def blocking_popen(command)
+        IO.popen(command) do |io|
+          io.read
+        end
+      end
+
       def mount_config_drive
-        mount_list_output = IO.popen('mount')
-        if !(mount_list_output.readlines =~ /#{@config_drive_mountpoint}/)
-          @logger.debug("Config drive not mounted at specified mountpoint.  Searching for config drive in #{@sys_block_dir_path}")
-          Dir.entries(@sys_block_dir_path).each do |device|
-            size_file_path = File.join(@sys_block_dir_path, device, 'size')
-            if File.exists?(size_file_path)
-              size_file = File.open(size_file_path, 'r')
-              # We're only detecting the device's size in sectors, there are other properties we could look for
-              # I.E. ro flag, fat32, removable flag
-              if size_file.read == "#{@config_drive_sector_count}"
-                @logger.debug("Found config drive as device #{device}, mounting.")
-                # TODO: Probably want to check for errors while mounting
-                IO.popen("mount -t fat32 /dev/#{device} #{@config_drive_mountpoint}")
+        if RightScale::Platform.windows?
+          # Finding devices (in windows) - http://stackoverflow.com/questions/3258518/ruby-get-available-disk-drives
+        else
+          mount_list_output = blocking_popen('mount')
+          if !(mount_list_output =~ /#{@config_drive_mountpoint}/)
+            @logger.debug("Config drive not mounted at specified mountpoint.  Searching for config drive in \"#{@sys_block_dir_path}\"")
+            Dir.entries(@sys_block_dir_path).each do |device|
+              size_file_path = File.join(@sys_block_dir_path, device, 'size')
+              if File.exists?(size_file_path)
+                device_size = File.open(size_file_path, 'r') {|f| f.read }
+                # We're only detecting the device's size in sectors, there are other properties we could look for
+                # I.E. ro flag, fat32, removable flag
+                if device_size == "#{@config_drive_sector_count}"
+                  @logger.debug("Found config drive as device \"#{device}\", mounting.")
+                  mount_output = blocking_popen("mount -t fat32 /dev/#{device} #{@config_drive_mountpoint}")
+                  unless $?.success?
+                    @logger.error("Unable to mount config drive to \"#{@config_drive_mountpoint}\" with device \"#{device}\"; Exit Status: #{$?.exitstatus}\nError: #{mount_output}")
+                  end
+                  return
+                end
               end
             end
+            # TODO: Should I be raising an exception here instead?
+            @logger.error("No config drive was found with a sector count of \"#{@config_drive_sector_count}\"")
           end
         end
 
