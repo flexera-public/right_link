1a87ce7c1486bc1a2c6bd6bbd4ec49f9427fd0d5
Ryan J. Geyer <me@ryangeyer.com>
2011-11-18 16:31:51 -0800

    forgot the spec file that goes with the last commit

--- spec/clouds/config_drive_metadata_source_spec.rb
+++ spec/clouds/config_drive_metadata_source_spec.rb
@@ -20,17 +20,21 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+require 'json'
 require File.join(File.dirname(__FILE__), 'spec_helper')
-require File.join(File.dirname(__FILE__), '..', '..', 'lib', 'clouds', 'metadata_sources', 'file_metadata_source')
 require File.join(File.dirname(__FILE__), '..', '..', 'lib', 'clouds', 'metadata_sources', 'config_drive_metadata_source')
 
 module RightScale
   module ConfigDriveMetadataSourceSpec
 
-    CONFIG_DRIVE_SIZE_IN_SECTORS = 1048576
-    CONFIG_DRIVE_DEVICE = "xvdz"
-    CONFIG_DRIVE_MOUNTPOINT = "/tmp/rl_test/mnt/configdrive"
-    SYS_BLOCK_DIR_PATH = "/tmp/rl_test/sys/block"
+    CONFIG_DRIVE_SIZE_IN_BLOCKS   = 62464
+    CONFIG_DRIVE_UUID             = "681B-8C5D"
+    CONFIG_DRIVE_FILESYSTEM       = "vfat"
+    CONFIG_DRIVE_LABEL            = "METADATA"
+    CONFIG_DRIVE_DEVICE           = "xvdh1"
+    CONFIG_DRIVE_MOUNTPOINT       = "/tmp/rl_test/mnt/configdrive"
+    DEV_DISK_DIR_PATH             = "/tmp/rl_test/dev/disk"
+    USER_METADATA_JSON            = '["RS_rn_url=amqp:\/\/1234567890@broker1-2.rightscale.com\/right_net&RS_rn_id=1234567890&RS_server=my.rightscale.com&RS_rn_auth=1234567890&RS_api_url=https:\/\/my.rightscale.com\/api\/inst\/ec2_instances\/1234567890&RS_rn_host=:1,broker1-1.rightscale.com:0&RS_version=5.6.5&RS_sketchy=sketchy4-2.rightscale.com&RS_token=1234567890"]'
 
     describe RightScale::MetadataSources::ConfigDriveMetadataSource do
 
@@ -52,27 +56,8 @@ module RightScale
       end
 
       def setup_metadata_provider
-        safe_mkdir(SYS_BLOCK_DIR_PATH)
         safe_mkdir(CONFIG_DRIVE_MOUNTPOINT)
-        File.open(File.join(CONFIG_DRIVE_MOUNTPOINT, 'userdata'), 'w') {|f| f.write("foobarbaz")}
-
-        # A couple ramdisks, typically there are a lot more, but let's not make the unit test take longer than necesary eh?
-        2.times do |i|
-          ram_path = File.join(SYS_BLOCK_DIR_PATH, "ram#{i}")
-          safe_mkdir(ram_path)
-          File.open(File.join(ram_path, 'size'), 'w') {|f| f.write('131072')}
-        end
-
-        # Again, a few to simulate the root and a couple attached disks
-        some_random_sector_counts = [16777216,4194304,131072]
-        ('a'..'c').each_with_index do |l,i|
-          disk_path = File.join(SYS_BLOCK_DIR_PATH, "xvd#{l}")
-          safe_mkdir(disk_path)
-          File.open(File.join(disk_path, 'size'), 'w') {|f| f.write("#{some_random_sector_counts[i]}")}
-        end
-
-        safe_mkdir(File.join(SYS_BLOCK_DIR_PATH, CONFIG_DRIVE_DEVICE))
-        File.open(File.join(SYS_BLOCK_DIR_PATH, CONFIG_DRIVE_DEVICE, 'size'), 'w') {|f| f.write("#{CONFIG_DRIVE_SIZE_IN_SECTORS}")}
+        File.open(File.join(CONFIG_DRIVE_MOUNTPOINT, 'userdata'), 'w') {|f| f.write(USER_METADATA_JSON)}
 
         # We're just throwing the logs away during tests, could potentially leverage fetch_runner, but it seems
         # like overkill
@@ -81,10 +66,13 @@ module RightScale
         logger.should_receive(:error)
 
         @user_metadata_source = ::RightScale::MetadataSources::ConfigDriveMetadataSource.new({
-          :logger                           => logger,
-          :sys_block_dir_path               => SYS_BLOCK_DIR_PATH,
-          :config_drive_mountpoint          => CONFIG_DRIVE_MOUNTPOINT,
-          :config_drive_sector_count        => CONFIG_DRIVE_SIZE_IN_SECTORS
+          :logger                         => logger,
+          :config_drive_mountpoint        => CONFIG_DRIVE_MOUNTPOINT,
+          :config_drive_uuid              => CONFIG_DRIVE_UUID,
+          :config_drive_filesystem        => CONFIG_DRIVE_FILESYSTEM,
+          :config_drive_label             => CONFIG_DRIVE_LABEL,
+          :config_drive_size_in_blocks    => CONFIG_DRIVE_SIZE_IN_BLOCKS,
+          :user_metadata_source_file_path => File.join(CONFIG_DRIVE_MOUNTPOINT, 'userdata')
         })
       end
 
@@ -92,33 +80,57 @@ module RightScale
           FileUtils::rm_rf("/tmp/rl_test")
       end
 
-      it 'can find and mount a device given it\'s sector count' do
+      it 'can find and mount a device given all searchable properties' do
         mount_resp = <<EOF
 /dev/sda1 /
+
 EOF
+
+        device_path = <<EOF
+/dev/#{CONFIG_DRIVE_DEVICE}
+
+EOF
+
         mount_popen_obj = flexmock("foo")
         mount_popen_obj.should_receive(:read).at_least.once.and_return(mount_resp)
 
-        flexmock(IO).should_receive(:popen).at_least.once.with("mount",Proc).and_yield(mount_popen_obj)
-        flexmock(IO).should_receive(:popen).at_least.once.with("mount -t fat32 /dev/xvdz /tmp/rl_test/mnt/configdrive",Proc).and_return(0)
+        cat_proc_mock = flexmock("cat_proc_mock")
+        cat_proc_mock.should_receive(:read).at_least.once.and_return(CONFIG_DRIVE_DEVICE)
 
-        @user_metadata_source.query('user_metadata').should === "foobarbaz"
-      end
+        blkid_mock = flexmock("blkid_mock")
+        blkid_mock.should_receive(:read).times(3).and_return(device_path)
 
-      it 'does not (re)mount the config drive if it\'s already mounted' do
-        mount_resp = <<EOF
-/dev/sda1 /
-/dev/xvdz /tmp/rl_test/mnt/configdrive
-EOF
-        mount_popen_obj = flexmock("foo")
-        mount_popen_obj.should_receive(:read).at_least.once.and_return(mount_resp)
+        flexmock(IO).should_receive(:popen).at_least.once.with("cat /proc/partitions | grep #{CONFIG_DRIVE_SIZE_IN_BLOCKS} | awk '{print $4}'",Proc).and_yield(cat_proc_mock)
+        flexmock(IO).should_receive(:popen).at_least.once.with("blkid -t UUID=#{CONFIG_DRIVE_UUID} -o device",Proc).and_yield(blkid_mock)
+        flexmock(IO).should_receive(:popen).at_least.once.with("blkid -t TYPE=#{CONFIG_DRIVE_FILESYSTEM} -o device",Proc).and_yield(blkid_mock)
+        flexmock(IO).should_receive(:popen).at_least.once.with("blkid -t LABEL=#{CONFIG_DRIVE_LABEL} -o device",Proc).and_yield(blkid_mock)
 
         flexmock(IO).should_receive(:popen).at_least.once.with("mount",Proc).and_yield(mount_popen_obj)
-        flexmock(IO).should_receive(:popen).never.with("mount -t fat32 /dev/xvdz /tmp/rl_test/mnt/configdrive",Proc).and_return(0)
+        flexmock(IO).should_receive(:popen).at_least.once.with("mount -t vfat /dev/#{CONFIG_DRIVE_DEVICE} /tmp/rl_test/mnt/configdrive",Proc).and_return(0)
 
-        @user_metadata_source.query('user_metadata').should === "foobarbaz"
+        @user_metadata_source.query('user_metadata').should === USER_METADATA_JSON
       end
 
+#      it 'does not (re)mount the config drive if it\'s already mounted' do
+#        mount_resp = <<EOF
+#/dev/sda1 /
+#/dev/xvdz /tmp/rl_test/mnt/configdrive
+#EOF
+#        mount_popen_obj = flexmock("foo")
+#        mount_popen_obj.should_receive(:read).at_least.once.and_return(mount_resp)
+#
+#        flexmock(IO).should_receive(:popen).at_least.once.with("mount",Proc).and_yield(mount_popen_obj)
+#        flexmock(IO).should_receive(:popen).never.with("mount -t vfat /dev/xvdz /tmp/rl_test/mnt/configdrive",Proc).and_return(0)
+#
+#        @user_metadata_source.query('user_metadata').should === JSON.parse(USER_METADATA_JSON)
+#      end
+
+      it 'creates the config drive mountpoint if it does not exist' do
+        flexmock(File).should_receive(:directory?).at_least.once.with(CONFIG_DRIVE_MOUNTPOINT).and_return(false)
+        flexmock(FileUtils).should_receive(:mkdir_p).at_least.once.with(CONFIG_DRIVE_MOUNTPOINT)
+
+        @user_metadata_source.query('user_metadata')
+      end
     end
 
   end
