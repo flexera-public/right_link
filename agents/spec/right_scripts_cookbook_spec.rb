ac9f7633e1d17a092aed1e85d6cfba889f03376e
Jon Seaberg <jon@rightscale.com>
2012-01-12 10:43:15 -0800

    pass the converge block to recursive calls of check_ohai

--- agents/spec/right_scripts_cookbook_spec.rb
+++ agents/spec/right_scripts_cookbook_spec.rb
@@ -57,7 +57,7 @@ describe RightScale::RightScriptsCookbook do
     recipes_dir = @cookbook.instance_variable_get(:@recipes_dir)
     recipe_path = File.join(recipes_dir, recipe_from_script(@script.nickname, @cookbook))
     recipe_content = IO.read("#{recipe_path}.rb")
-    regexp = "^right_script '#{@script.nickname}' do\n"
+    regexp = "\b*right_script '#{@script.nickname}' do\n"
     regexp += "^  #{Regexp.escape("parameters(node[\"#{@script.nickname}\"][\"parameters\"])")}\n"
     regexp += "^  cache_dir +'#{Regexp.escape(@cookbook.cache_dir(@script))}'\n"
     regexp += "^  source_file +'#{recipe_path}'\n"
