1d843f2e062b0d38aecedbdc05ad65b8a3bae502
Ryan Williamson <ryan.williamson@rightscale.com>
2012-03-01 18:51:14 -0800

    Added Logging for the nil thread name case

--- lib/instance/cook/executable_sequence.rb
+++ lib/instance/cook/executable_sequence.rb
@@ -81,6 +81,8 @@ module RightScale
     # === Parameter
     # bundle(RightScale::ExecutableBundle):: Bundle to be run
     def initialize(bundle)
+      
+      Log.warning("Encountered a nil thread name unexpectedly, defaulting to '#{RightScale::AgentConfig.default_thread_name}'") unless bundle.thread_name
       bundle.thread_name ||= RightScale::AgentConfig.default_thread_name
       
       unless bundle.thread_name =~ RightScale::AgentConfig.valid_thread_name
