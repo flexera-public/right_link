839bce74a35d9091aa46c862427c55ff3b249c94
Ryan Williamson <ryan.williamson@rightscale.com>
2012-03-07 15:37:07 -0800

    Implemented finding absolute path to usermod as rightscale user

--- lib/instance/login_user_manager.rb
+++ lib/instance/login_user_manager.rb
@@ -90,7 +90,6 @@ module RightScale
       # Can't use executable? because the rightscale user can't execute useradd without sudo
       useradd = ['/usr/bin/useradd', '/usr/sbin/useradd', '/bin/useradd', '/sbin/useradd'].select { |key| File.exists? key }.first
       raise RightScale::LoginManager::SystemConflict, "Failed to find a suitable implementation of 'useradd'." unless useradd
-      
       %x(sudo #{useradd} -s /bin/bash -u #{uid} -m #{Shellwords.escape(username)})
 
       case $?.exitstatus
@@ -141,6 +140,9 @@ module RightScale
 
       groups   = Shellwords.escape(groups.to_a.join(','))
       username = Shellwords.escape(username)
+      # Can't use executable? because the rightscale user can't execute usermod without sudo
+      usermod = ['/usr/bin/usermod', '/usr/sbin/usermod', '/bin/usermod', '/sbin/usermod'].select { |key| File.exists? key }.first
+      raise RightScale::LoginManager::SystemConflict, "Failed to find a suitable implementation of 'usermod'." unless usermod
       output = %x(sudo usermod -G #{groups} #{username})
 
       case $?.exitstatus
