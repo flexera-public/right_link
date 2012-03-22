fab241c325029bb71ac093a88e40b86412192a33
Ryan Williamson <ryan.williamson@rightscale.com>
2012-03-22 14:58:02 -0700

    Added Policy and PolicyManager classes

---
+++ lib/instance/policy_manager.rb
@@ -0,0 +1,107 @@
+#
+# Copyright (c) 2009-2012 RightScale Inc
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+module RightScale
+
+  # Manages policies
+  class PolicyManager
+    # Hash of policy classes keyed by policy name
+    @policy = Hash.new
+    
+    # Accessor method for policy hash
+    def self.policy
+      @policy
+    end
+    
+    # Signals the successful execution of a recipe with the given policy name
+    #
+    # === Parameters
+    # policy_name(String):: name of the policy
+    #
+    # === Return
+    # true:: Always return true
+    def self.success(policy_name)
+      policy = self.get_policy(policy_name)
+      policy.count = policy.count + 1
+      true
+    end
+    
+    # Signals the failed execution of a recipe with the given policy name
+    #
+    # === Parameters
+    # policy_name(String):: name of the policy
+    #
+    # === Return
+    # true:: Always return true
+    def self.fail(policy_name)
+      self.get_policy(policy_name).count = 0
+      true
+    end
+    
+    # Returns the audit ID associated with the given policy
+    #
+    # === Parameters
+    # policy_name(String):: name of the policy
+    #
+    # === Return
+    # result(Integer):: Audit ID associated with this policy
+    def self.audit_id_for(policy_name)
+      self.get_policy(policy_name).audit_id
+    end
+    
+    # Returns the timestamp of the last audit of a recipe with the given policy name
+    #
+    # === Parameters
+    # policy_name(String):: name of the policy
+    #
+    # === Return
+    # result(Integer):: UNIX Timestamp
+    def self.last_audit_for(policy_name)
+      self.get_policy(policy_name).audit_timestamp
+    end
+    
+    # Returns the number of successful runs of the recipe with the given policy name since the last time it audited
+    #
+    # === Parameters
+    # policy_name(String):: name of the policy
+    #
+    # === Return
+    # result(Integer):: UNIX Timestamp
+    def self.success_count_for(policy_name)
+      self.get_policy(policy_name).count
+    end
+    
+    
+    # Returns the policy associated with the given policy name
+    #
+    # === Parameters
+    # policy_name(String):: name of the policy
+    #
+    # === Return
+    # result(Policy):: Policy object
+    def self.get_policy(policy_name)
+      self.policy.has_key?(policy_name) ? self.policy[policy_name] : self.policy[policy_name] = Policy.new(policy_name)
+    end
+    
+  end
+
+end
---
+++ spec/instance/policy_manager_spec.rb
@@ -0,0 +1,111 @@
+#
+# Copyright (c) 2009-2012 RightScale Inc
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
+Spec::Matchers.define :be_the_same_policy_as do |expected|
+  match do |actual|
+    expected.audit_id.should        == actual.audit_id
+    expected.count.should           == actual.count
+    expected.audit_timestamp.should == actual.audit_timestamp
+  end
+end
+
+describe RightScale::PolicyManager do
+  
+  before(:each) do
+    @policy_name = "policy_name"
+  end
+  
+  context :success do
+    it 'should increment the policy success count' do
+      RightScale::PolicyManager.success(@policy_name)
+      RightScale::PolicyManager.get_policy(@policy_name).count.should == 1
+    end
+    
+    it 'should return true' do
+      RightScale::PolicyManager.success(@policy_name).should == true
+    end
+    
+  end
+  
+  context :fail do
+    it 'should reset the policy success count' do
+      RightScale::PolicyManager.get_policy(@policy_name).count = 1
+      RightScale::PolicyManager.fail(@policy_name)
+      RightScale::PolicyManager.get_policy(@policy_name).count.should == 0
+    end
+    
+    it 'should return true' do
+      RightScale::PolicyManager.success(@policy_name).should == true
+    end
+  end
+  
+  context :audit_id_for do
+    it 'returns the audit ID for the policy' do
+      audit_id = 1
+      policy = RightScale::PolicyManager.get_policy(@policy_name)
+      policy.audit_id = audit_id
+      
+      RightScale::PolicyManager.audit_id_for(@policy_name).should == audit_id
+    end
+  end
+  
+  context :last_audit_for do
+    it 'returns the timestamp for the last audit for the policy' do
+      audit_timestamp = Time.now
+      policy = RightScale::PolicyManager.get_policy(@policy_name)
+      policy.audit_timestamp = audit_timestamp
+      
+      RightScale::PolicyManager.last_audit_for(@policy_name).should == audit_timestamp
+    end
+  end
+  
+  context :success_count_for do
+    it 'returns the success count for the policy' do
+      count = 5
+      policy = RightScale::PolicyManager.get_policy(@policy_name)
+      policy.count = count
+      
+      RightScale::PolicyManager.success_count_for(@policy_name).should == count
+    end
+  end
+  
+  context :get_policy do
+    it 'returns a new policy if one does not already exist' do
+      policy_name = "policy_#{rand(5)}"
+      expected_policy = RightScale::Policy.new(policy_name)
+      actual_policy = RightScale::PolicyManager.get_policy(policy_name)
+      
+      actual_policy.should be_the_same_policy_as(expected_policy)
+    end
+    
+    it 'returns a policy if one already exists' do
+      expected_policy = RightScale::PolicyManager.get_policy(@policy_name)
+      expected_policy.audit_id = 5
+      actual_policy = RightScale::PolicyManager.get_policy(@policy_name)
+      
+      actual_policy.should be_the_same_policy_as(expected_policy)
+    end
+  end
+  
+end
