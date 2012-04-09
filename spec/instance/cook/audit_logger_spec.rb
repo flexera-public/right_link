5a25d310b22710ef3c54bda782eb3b22a6e47be7
Tony Spataro <tony@rightscale.com>
2012-04-09 16:17:05 -0700

    Refine test coverage; add fixtures captured from legit instances; fix a failing test discovered by tests (woot TDD)

--- spec/instance/cook/audit_logger_spec.rb
+++ spec/instance/cook/audit_logger_spec.rb
@@ -25,6 +25,19 @@ require 'right_agent/log'
 require File.normalize_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'lib', 'instance', 'cook', 'audit_stub'))
 
 describe RightScale::AuditLogger do
+  LOG_FIXTURES = {}
+
+  # Load some fixtures from disk, that comprise some log file excerpts captured from actual RightLink instances.
+  # The fixtures are stored in memory as a hash, where the key is the OS on which the log output
+  # was captured ('windows' or 'linux') and the values are an ordered sequence of individual log entries,
+  # each which may consist of multiple lines.
+  fixtures_dir = File.expand_path('../fixtures/error_logs', __FILE__)
+  ['linux', 'windows'].each do |os|
+    LOG_FIXTURES[os] = []
+    Dir.glob(File.join(fixtures_dir, "right_script_failure.#{os}.*.txt")).sort.each do |file|
+      LOG_FIXTURES[os] << File.read(file)
+    end
+  end
 
   before(:each) do
     @auditor = flexmock(RightScale::AuditStub.instance)
@@ -50,38 +63,16 @@ describe RightScale::AuditLogger do
     @logger.fatal
   end
 
-  execution_errors = {
-    'RightScriptProvider (Linux)' => [
-      "RightScript[TonyS Always Fail] (/var/cache/rightscale/right_scripts/default/right_scripts_cookbook/recipes/TonyS_Always_Fail.rb:1:in `from_file') had an error:\n" +
-      "RightScript[TonyS Always Fail] (right_scripts_cookbook::TonyS_Always_Fail line 1) had an error: RightScale::Exceptions::Exec: RightScript < TonyS Always Fail > exited with 42\n" +
-      "/opt/rightscale/right_link/lib/chef/providers/right_script_provider.rb:137:in `action_run'",
-      "RightScript[TonyS Always Fail] (right_scripts_cookbook::TonyS_Always_Fail line 1) had an error: RightScale::Exceptions::Exec: RightScript < TonyS Always Fail > exited with 42"
-    ],
-    #TODO capture an actual Windows RightScript failure and put it here instead
-    'RightScriptProvider (Windows)' => [
-      "RightScript[TonyS Always Fail] (/var/cache/rightscale/right_scripts/default/right_scripts_cookbook/recipes/TonyS_Always_Fail.rb:1:in `from_file') had an error:\n" +
-      "RightScript[TonyS Always Fail] (right_scripts_cookbook::TonyS_Always_Fail line 1) had an error: RightScale::Exceptions::Exec: RightScript < TonyS Always Fail > exited with 42\n" +
-      "/opt/rightscale/right_link/lib/chef/providers/right_script_provider.rb:137:in `action_run'",
-      "RightScript[TonyS Always Fail] (right_scripts_cookbook::TonyS_Always_Fail line 1) had an error: RightScale::Exceptions::Exec: RightScript < TonyS Always Fail > exited with 42"
-    ],
-    #TODO capture an actual PowershellProvider failure and put it here instead!
-    'PowershellProvider'  => [
-      "db_sqlserver_database[app_test] (C:/PROGRA~1/RIGHTS~1/SandBox/Ruby/lib/ruby/gems/1.8/gems/chef-0.8.16.3/lib/chef/mixin/recipe_definition_dsl_core.rb line 59) had an error:\nUnexpected exit code from action. Expected 0 but returned 1.  Script: C:/DOCUME~1/ALLUSE~1/APPLIC~1/RIGHTS~1/cache/RIGHTS~1/COOKBO~1/3222BD~1/repo/COOKBO~1/DB_SQL~1/POWERS~1/database/run_command.ps1\n" +
-      "RightScript[TonyS Always Fail] (right_scripts_cookbook::TonyS_Always_Fail line 1) had an error: RightScale::Exceptions::Exec: RightScript < TonyS Always Fail > exited with 42\n"+
-      "/opt/rightscale/right_link/lib/chef/providers/right_script_provider.rb:137:in `action_run'",
-      "An external command returned an error during the execution of Chef:\n\nUnexpected exit code from action. Expected 0 but returned 1.  Script: C:/DOCUME~1/ALLUSE~1/APPLIC~1/RIGHTS~1/cache/RIGHTS~1/COOKBO~1/3222BD~1/repo/COOKBO~1/DB_SQL~1/POWERS~1/database/run_command.ps1"
-    ],
-  }
-
-  execution_errors.each_pair do |source, errors|
-    it "should filter script execution errors logged by #{source}" do
-      @auditor.should_receive(:append_error).once
-
-      chef_message = errors.first
-      executable_sequence_message = errors.last
-
-      @logger.error(chef_message)  # Chef attempts to log but is filtered
-      @logger.error(executable_sequence_message)  # executable_sequence is not filtered
+  # Dynamically generate some test cases for log filtering based on the fixtures we have
+  LOG_FIXTURES.each_pair do |os, log_entries|
+    context "under #{os}" do
+      it "should filter RightScript errors from audit output" do
+        pending('missing fixtures') if log_entries.empty?
+        @auditor.should_receive(:append_error).twice
+        log_entries.each { |line|
+          @logger.error(line)
+        }
+      end
     end
   end
 
