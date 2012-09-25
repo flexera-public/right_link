833bc867499a2dfe9888da649735201602ca8306
Scott Messier <scott@rightscale.com>
2012-09-24 18:04:43 -0700

    fixes for finding cloud extension scripts relative to Platform.filesystem.private_bin_dir, etc.

--- lib/clouds/clouds/ec2.rb
+++ lib/clouds/clouds/ec2.rb
@@ -34,8 +34,8 @@ metadata_writers 'metadata_writers/dictionary_metadata_writer',
 # Assembles the command line needed to regenerate cloud metadata on demand.
 def cloud_metadata_generation_command
   ruby_path = File.normalize_path(AgentConfig.sandbox_ruby_cmd)
-  rs_cloud_path = File.normalize_path(File.join(AgentConfig.parent_dir, 'right_link', 'bin', 'cloud'))
-  return "#{ruby_path} #{rs_cloud_path} --quiet --action write_cloud_metadata"
+  rs_cloud_path = File.normalize_path(Gem.bin_path('right_link', 'cloud'))
+  return "#{ruby_path} #{rs_cloud_path} --action write_cloud_metadata"
 end
 
 # Selects metadata from multiple sources in support of serverizing existing
