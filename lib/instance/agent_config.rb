dee257f6420fd3f50b23bba43973598c77f2d559
Stas Turlo <stanislav.turlo@rightscale.com>
2012-10-25 15:31:43 +0300

    acu52495 add right_agent reference

--- lib/instance/agent_config.rb
+++ lib/instance/agent_config.rb
@@ -20,6 +20,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 require 'right_support'
+require 'right_agent'
 
 module RightScale
   if Platform.linux?
