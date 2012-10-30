9171152c5fff2c037941f0fd32ed502f0da91e40
Tony Spataro <tony @rightscale.com>
2012-10-29 19:38:07 -0700

    acu43036 - move example method from Platform to AgentConfig.

--- lib/instance/platform/linux.rb
+++ lib/instance/platform/linux.rb
@@ -2,11 +2,6 @@ module RightScale
   # Linux specific implementation
   class Platform
     class Filesystem
-      # Static (time-invariant) state that is specific to RightLink
-      def right_link_static_state_dir
-        '/etc/rightscale.d/right_link'
-      end
-
       # Dynamic, persistent runtime state that is specific to RightLink
       def right_link_dynamic_state_dir
         '/var/lib/rightscale/right_link'
