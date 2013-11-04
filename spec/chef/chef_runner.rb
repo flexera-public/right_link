41d83e16786350110dee9e5f0c0bb4b149b977ec
Scott Messier <scott@rightscale.com>
2013-11-04 14:13:18 -0800

    acu131387 defeat chef logging to console during spec runs (using our fixed version of NullFormatter)

--- spec/chef/chef_runner.rb
+++ spec/chef/chef_runner.rb
@@ -179,6 +179,9 @@ EOF
                                              params[:args][:cwd])
         end
 
+        # defeat chef logging to console during spec runs.
+        ::Chef::Config.add_formatter(:null)
+
         # must set file cache path and ensure it exists otherwise evented run_command will fail
         cache_dir_path = File.join(::RightScale::Platform.filesystem.temp_dir, 'chef_runner_1B0C7CAA87E241daB90B75829DD6A833')
         AgentConfig.cache_dir = cache_dir_path
