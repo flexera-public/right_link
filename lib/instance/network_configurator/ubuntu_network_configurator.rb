fc5de49308b9606d6f834669ca7bd4920afbb51c
Stas Turlo <stanislav.turlo@rightscale.com>
2014-02-04 10:06:00 +0300

    acu141082 NAT routing support for Ubuntu 12.04

--- lib/instance/network_configurator/ubuntu_network_configurator.rb
+++ lib/instance/network_configurator/ubuntu_network_configurator.rb
@@ -11,6 +11,8 @@ module RightScale
     def enable_separate_configs
       File.open("/etc/network/interfaces", "w") do |config|
         config.write <<-EOH
+# File managed by RightScale
+# DO NOT EDIT
 auto lo
 iface lo inet loopback
 
@@ -27,13 +29,36 @@ source /etc/network/interfaces.d/*.cfg
 
     def config_data(device, ip, netmask, gateway, nameservers)
       config_data = <<-EOH
-      auto #{device}
+# File managed by RightScale
+# DO NOT EDIT
+auto #{device}
 iface #{device} inet static
 address #{ip}
 netmask #{netmask}
 gateway #{gateway}
 dns-nameservers #{nameservers.join(" ")}
       EOH
+    end
+
+    def ip_route_cmd(network, nat_server_ip)
+      "up ip route add #{network} via #{nat_server_ip}"
+    end
+
+    def init_device_config_file(file_path, device)
+      File.open(file_path, "w") do |config|
+        config.write <<-EOH
+# File managed by RightScale
+# DO NOT EDIT
+auto #{device}
+iface #{device} inet dhcp
+        EOH
+      end
+    end
+
+    def routes_file(device)
+      routes_file = config_file(device)
+      init_device_config_file(routes_file, device) unless File.exists?(routes_file)
+      routes_file
     end
   end
 end
