39138ae75a14ce8349b9aff148e18e2867b67da1
Stas Turlo <stanislav.turlo@rightscale.com>
2014-02-06 21:36:32 +0300

    acu152174 add ::Ohai::Mixin::RightLink::CloudUtilities specs

--- spec/chef/ohai/mixin/rightlink_spec.rb
+++ spec/chef/ohai/mixin/rightlink_spec.rb
@@ -48,7 +48,49 @@ describe ::Ohai::Mixin::RightLink do
 
 
   context 'CloudUtilities' do
+    subject { Object.new.extend(::Ohai::Mixin::RightLink::CloudUtilities) }
+    let(:network) {
+      {
+        :interfaces => {
+          :lo   => { :addresses => { "127.0.0.1" => { 'family' => 'inet' } }, :flags => ["LOOPBACK"] },
+          :eth0 => { :addresses => { "50.23.101.210" => { 'family' => 'inet' } }, :flags => [] },
+          :eth1 => { :addresses => { "192.168.0.1" => { 'family' => 'inet' } }, :flags => []}
+        }
+      }
+    }
     it 'should query instance ip'
+
+    context '#private_ipv4?' do
+      it 'returns true for private ipv4' do
+        subject.private_ipv4?("192.168.0.1").should be_true
+      end
+
+      it 'returns false for public ipv4' do
+        subject.private_ipv4?("8.8.8.8").should_not be_true
+      end
+    end
+
+    context '#ips' do
+      it 'returns list of ips of all network interfaces' do
+        subject.ips(network).should_not be_empty
+      end
+
+      it "doesn't include localhost ip to list" do
+        subject.ips(network).should_not include "127.0.0.1"
+      end
+    end
+
+    context '#public_ips' do
+      it 'returns list of public ips' do
+        subject.public_ips(network).any? { |ip| subject.private_ipv4?(ip) }.should_not be true
+      end
+    end
+
+    context '#private_ips' do
+      it 'returns list of private ips' do
+        subject.private_ips(network).all? { |ip| subject.private_ipv4?(ip) }.should be true
+      end
+    end
   end
 
   context 'DirMetadata' do
