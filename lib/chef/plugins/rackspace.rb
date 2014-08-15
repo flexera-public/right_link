7aa6f8361f840212e3f04d91586628248b766511
Peter Schroeter <peter.schroeter@rightscale.com>
2014-08-14 17:28:58 -0700

    acu179610 Refactor rackspace plugin to read from the xenstore directly

--- lib/chef/plugins/rackspace.rb
+++ lib/chef/plugins/rackspace.rb
@@ -14,10 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require 'json'
+
 provides "rackspace"
 
 require_plugin "kernel"
-require_plugin "network"
 
 def on_windows?
   RUBY_PLATFORM =~ /windows|cygwin|mswin|mingw|bccwin|wince|emx/
@@ -32,20 +33,39 @@ def has_rackspace_kernel?
   kernel[:release].split('-').last.eql?("rscloud")
 end
 
+def xenstore_command(command, args)
+  if on_windows?
+    client = "\"c:\\Program Files\\Citrix\\XenTools\\xenstore_client.exe\""
+    if command == "ls"
+      command = "dir"
+    end
+  else
+    client = "xenstore"
+  end
+  command = "#{client} #{command} #{args}"
+  begin
+    status, stdout, stderr = run_command(:no_status_check => true, :command => command)
+    status = status.exitstatus if status.respond_to?(:exitstatus)
+  rescue Ohai::Exceptions::Exec => e
+    Ohai::Log.debug("xenstore command '#{command}' failed (#{e.class}: #{e.message})")
+    stdout = stderr = nil
+    status = 1
+  end
+  [status, stdout, stderr]
+end
+
 # Checks for rackspace provider attribute
 #
 # === Return
 # true:: If rackspace provider attribute found
 # false:: Otherwise
 def has_rackspace_metadata?
-  client = "\"c:\\Program Files\\Citrix\\XenTools\\xenstore_client.exe\""
-  command = on_windows? : "#{client} read" : "xenstore-read"
-  status, stdout, stderr = run_command(:no_status_check => true, :command => "#{command} vm-data/provider_data/provider")
+  status, stdout, stderr = xenstore_command("read", "vm-data/provider_data/provider")
   if status == 0
     stdout.strip.downcase == 'rackspace'
+  else
+    false
   end
-rescue Ohai::Exceptions::Exec
-  false
 end
 
 # Identifies the rackspace cloud
@@ -57,81 +77,126 @@ def looks_like_rackspace?
   hint?('rackspace') || has_rackspace_metadata? || has_rackspace_kernel?
 end
 
-def get_interface_addresses(eth)
-  return network[:interfaces][eth][:addresses] unless on_windows?
-
-  connection = (eth == :eth0 ? 'public' : 'private')
-  _,params = network[:interfaces].select {|_,interface| interface[:instance][:net_connection_id] == connection }.first
-  params[:addresses]
-end
-
-# Names rackspace ip address
-#
-# === Parameters
-# name<Symbol>:: Use :public_ip or :private_ip
-# eth<Symbol>:: Interface name of public or private ip
-def get_ip_address(name, eth)
-  get_interface_addresses(eth).each do |key, info|
-    if info['family'] == 'inet'
-      rackspace[name] = key
-      break # break when we found an address
+# Grab a list of rackspace interfaces from the xenstore. The interface definitions
+# are encoded using JSON. 
+# Example for a public interface given below
+# {
+#     "broadcast": "162.209.6.255",
+#     "dns": [
+#         "173.203.4.9",
+#         "173.203.4.8"
+#     ],
+#     "gateway": "162.209.6.1",
+#     "gateway_v6": "fe80::def",
+#     "ip6s": [
+#         {
+#             "enabled": "1",
+#             "gateway": "fe80::def",
+#             "ip": "2001:4801:7819:74:be76:4eff:fe11:1553",
+#             "netmask": 64
+#         }
+#     ],
+#     "ips": [
+#         {
+#             "enabled": "1",
+#             "gateway": "162.209.6.1",
+#             "ip": "162.209.6.148",
+#             "netmask": "255.255.255.0"
+#         }
+#     ],
+#     "label": "public",
+#     "mac": "BC:76:4E:11:15:53"
+# }
+# Example for a private interface given below:
+# {
+#     "broadcast": "10.178.127.255",
+#     "dns": [
+#         "173.203.4.9",
+#         "173.203.4.8"
+#     ],
+#     "gateway": null,
+#     "ips": [
+#         {
+#             "enabled": "1",
+#             "gateway": null,
+#             "ip": "10.178.6.80",
+#             "netmask": "255.255.128.0"
+#         }
+#     ],
+#     "label": "private",
+#     "mac": "BC:76:4E:11:17:2E",
+#     "routes": [
+#         {
+#             "gateway": "10.178.0.1",
+#             "netmask": "255.240.0.0",
+#             "route": "10.208.0.0"
+#         },
+#         {
+#             "gateway": "10.178.0.1",
+#             "netmask": "255.240.0.0",
+#             "route": "10.176.0.0"
+#         }
+#     ]
+# }
+def get_rackspace_interfaces
+  return @rackspace_interfaces if @rackspace_interfaces
+  @rackspace_interfaces = []
+  status, stdout, stderr = xenstore_command("ls", "vm-data/networking")
+  if status == 0
+    stdout.split("\n").each do |line|
+      id = line.split.first
+      status, stdout, stderr = xenstore_command("read", "vm-data/networking/#{id}")
+      if status == 0
+        begin
+          interface = JSON.parse(stdout)
+          @rackspace_interfaces << interface
+        rescue Exception => e
+          Ohai::Log.debug("Unable to parse Rackspace interface definition for #{id}: #{e.message} on (#{stdout})")
+        end
+      else
+        Ohai::Log.debug("Unable to query xen-store for interface #{id}: #{status} (#{stderr})")
+      end
     end
+  else
+    Ohai::Log.debug("Unable to query xen-store for list of interfaces: #{status} (#{stderr})")
   end
+  @rackspace_interfaces
 end
 
-# Names rackspace ipv6 address for interface
-#
-# === Parameters
-# name<Symbol>:: Use :public_ip or :private_ip
-# eth<Symbol>:: Interface name of public or private ip
-def get_global_ipv6_address(name, eth)
-  get_interface_addresses(eth).each do |key, info|
-    # check if we got an ipv6 address and if its in global scope
-    if info['family'] == 'inet6' && info['scope'] == 'Global'
-      rackspace[name] = key
-      break # break when we found an address
-    end
-  end
+def cull_ips(ips)
+  ips.map { |ip| ip["ip"] }
 end
 
-# Get the rackspace region on linx
+# Get the rackspace region
 #
 def get_region()
-  rackspace[:region] = on_windows? ? get_region_win : get_region_linux
-rescue Ohai::Exceptions::Exec
-  Ohai::Log.debug("Unable to find xenstore client, cannot capture region information for Rackspace cloud")
-end
-
-# Get the rackspace region on linx
-#
-def get_region_linux()
-  status, stdout, stderr = run_command(:no_status_check => true, :command => "xenstore-ls vm-data/provider_data")
-  if status.success?
-    stdout.split("\n").each do |line|
-     return line.split[2].delete('\"') if line =~ /^region/
-    end
+  status, stdout, stderr = xenstore_command("read", "vm-data/provider_data/region")
+  if status == 0
+    stdout.strip
+  else
+    Ohai::Log.debug("could not read region information for Rackspace cloud from xen-store")
+    nil
   end
-end
-
-# Get the rackspace region on Windows
-#
-def get_region_win()
-  client = "\"c:\\Program Files\\Citrix\\XenTools\\xenstore_client.exe\""
-  status, stdout, stderr = run_command(:no_status_check => true, :command => "#{client} read vm-data/provider_data/region")
-  return stdout.strip if status.success?
 end
 
 # Adds rackspace Mash
 if looks_like_rackspace?
   rackspace Mash.new
-  get_ip_address(:public_ip, :eth0)
-  get_ip_address(:private_ip, :eth1)
-  get_region()
+  public_interfaces = get_rackspace_interfaces.select { |iface| iface["label"] == "public" }
+  private_interfaces = get_rackspace_interfaces.select { |iface| iface["label"] == "private" }
+  rackspace[:public_ips] = public_interfaces.map { |iface| cull_ips(iface["ips"]) }.flatten
+  rackspace[:private_ips] = private_interfaces.map { |iface| cull_ips(iface["ips"]) }.flatten
+  rackspace[:public_ipv6s] = public_interfaces.map { |iface| cull_ips(iface["ip6s"]) }.flatten
+
+  rackspace[:public_ip] = rackspace[:public_ips].first
+  rackspace[:private_ip] = rackspace[:private_ips].first
+  rackspace[:public_ipv4] = rackspace[:public_ips].first
+  rackspace[:public_ipv6] = rackspace[:public_ipv6s].first
+  rackspace[:local_ipv4] = rackspace[:private_ips].first
+
+  rackspace[:region] = get_region()
+
   # public_ip + private_ip are deprecated in favor of public_ipv4 and local_ipv4 to standardize.
-  rackspace[:public_ipv4] = rackspace[:public_ip]
-  get_global_ipv6_address(:public_ipv6, :eth0)
   rackspace[:public_hostname] = "#{rackspace[:public_ip].gsub('.','-')}.static.cloud-ips.com"
-  rackspace[:local_ipv4] = rackspace[:private_ip]
-  get_global_ipv6_address(:local_ipv6, :eth1)
   rackspace[:local_hostname] = hostname
 end
