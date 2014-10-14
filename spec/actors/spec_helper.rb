43d31d0f4b9d3843d7ff4d5433b1217198662f77
Tony Spataro <tony@rightscale.com>
2014-10-14 11:46:59 -0700

    acu183994 fix relative requires in specs so the specs don’t rely on PWD having a certain value.

--- spec/actors/spec_helper.rb
+++ spec/actors/spec_helper.rb
@@ -20,6 +20,6 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
+require File.expand_path('../../spec_helper', __FILE__)
 require File.normalize_path(File.join(File.dirname(__FILE__), '..', '..', 'lib', 'instance'))
 require File.normalize_path(File.join(File.dirname(__FILE__), '..', '..', 'lib', 'chef', 'right_providers'))
