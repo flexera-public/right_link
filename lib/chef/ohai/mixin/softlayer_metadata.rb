a67bf2a73c5fa0ff47972657fa92c718c63c6dba
Peter Schroeter <peter.schroeter@rightscale.com>
2014-11-20 16:05:39 -0800

    acu186443 Fix certification issues with Softlayer metadata api
    Use our bundle ca cert

--- lib/chef/ohai/mixin/softlayer_metadata.rb
+++ lib/chef/ohai/mixin/softlayer_metadata.rb
@@ -1,25 +1,41 @@
-require 'net/http'
+require 'net/https'
 
+# http://sldn.softlayer.com/reference/services/SoftLayer_Resource_Metadata
 module ::Ohai::Mixin::SoftlayerMetadata
-  SOFTLAYER_API_QUERY_URL='https://api.service.softlayer.com/rest/v3'
+  SOFTLAYER_API_QUERY_URL='https://api.service.softlayer.com/rest/v3.1/SoftLayer_Resource_Metadata'
 
   def fetch_metadata
     metadata  = {
-      'public_fqdn' => api_query("SoftLayer_Resource_Metadata/getFullyQualifiedDomainName.txt"),
-      'local_ipv4' => api_query("SoftLayer_Resource_Metadata/getPrimaryBackendIpAddress.txt"),
-      'public_ipv4' => api_query("SoftLayer_Resource_Metadata/getPrimaryIpAddress.txt")
+      'public_fqdn'   => fetch_metadata_item("getFullyQualifiedDomainName.txt"),
+      'local_ipv4'    => fetch_metadata_item("getPrimaryBackendIpAddress.txt"),
+      'public_ipv4'   => fetch_metadata_item("getPrimaryIpAddress.txt"),
+      'region'        => fetch_metadata_item("getDatacenter.txt"),
+      'instance_id'   => fetch_metadata_item("getId.txt")
     }
+
     metadata
   end
 
-  def api_query(query)
+  # We ship curl's CA bundle with rightlink. 
+  def ca_file_location
+    File.expand_path("../../../../instance/cook/ca-bundle.crt", __FILE__)
+  end
+
+  def fetch_metadata_item(item)
     begin
-      u = URI("#{SOFTLAYER_API_QUERY_URL}/#{query}")
-      res = Net::HTTP.start(u.hostname, u.port, :use_ssl => true) {|http|
-        req = Net::HTTP::Get.new u.request_uri
-        res = http.request(req)
-      }
-      result = res.body
+      full_url = "#{SOFTLAYER_API_QUERY_URL}/#{item}"
+      u = URI(full_url)
+      net = Net::HTTP.new(u.hostname, u.port)
+      net.ssl_version = "TLSv1"
+      net.use_ssl = true
+      net.ca_file = ca_file_location
+      res = net.request(Net::HTTP::Get.new(u.request_uri))
+      if res.code.to_i.between?(200,299) 
+        res.body
+      else
+        ::Ohai::Log.error("Unable to fetch item #{full_url}: status (#{res.code}) body (#{res.body})")
+        ""
+      end
     rescue Exception => e
       ::Ohai::Log.error("Unable to fetch softlayer metadata from #{u}: #{e.class}: #{e.message}")
       nil
